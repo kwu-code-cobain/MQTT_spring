4b12b88588f8e7ac8a21798185d438126ed63224
kymin <park48664866@gmail.com>
2024-10-10 09:13:29 +0900

    initial commit

---
+++ src/main/java/com/codecobain/mqtt_spring/MqttSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codecobain.mqtt_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttSpringApplication.class, args);
+	}
+
+}
