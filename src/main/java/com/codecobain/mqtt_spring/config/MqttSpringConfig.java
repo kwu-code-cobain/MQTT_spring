4cf736085cfb57b3a0ec5b16d00bba33a1868575
kymin <park48664866@gmail.com>
2024-10-13 12:08:39 +0900

    feat: 브로커 연결, publish 구현
    브로커 연결:
    - MQTT통신을 위한 브로커에 클라이언트로 연결
    - 브로커의 ip, port와 클라이언트의 id 설정
    
    publish 구현:
    - 클라이언트를 이용하여 연결된 브로커에 메시지를 publish
    - 토픽 설정으로 특정 사용자에게만 메시지 전달

---
+++ src/main/java/com/codecobain/mqtt_spring/config/MqttSpringConfig.java
@@ -0,0 +1,28 @@
+package com.codecobain.mqtt_spring.config;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.beans.factory.annotation.Value;
+
+@EnableAsync
+@Configuration
+public class MqttSpringConfig {
+    // MQTT 클라이언트를 빈으로 등록하고 브로커와 연결
+    @Bean
+    public MqttClient mqttClient(@Value("${mqtt.clientId}") String clientId, @Value("${mqtt.hostname}") String hostname,
+            @Value("${mqtt.port}") int port)
+            throws MqttException {
+        MqttClient mqttClient = new MqttClient("tcp://" + hostname + ":" + port, clientId);
+
+        try {
+            mqttClient.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mqttClient;
+    }
+}
